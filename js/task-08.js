e0ac5292436b889b7f20b0c12ecc54e8a9e89008
yarqui <y.pelykh@gmail.com>
2023-12-06 20:33:23 -0500

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector("form.login-form");
 
+const submitForm = (e) => {
+  e.preventDefault();
+
+  const formInputs = [...form.elements].filter((el) => el.nodeName === "INPUT");
+
+  const hasEmptyInputs = formInputs.some((el) => !el.value);
+
+  if (hasEmptyInputs) {
+    return alert("All inputs should be filled");
+  }
+
+  const userCredentials = formInputs.reduce(
+    (userCredentials, { name, value }) => {
+      userCredentials[name] = value;
+      return userCredentials;
+    },
+    {}
+  );
+
+  console.log("userCredentials", userCredentials);
+
+  form.reset();
+  document.activeElement.blur();
+};
+
+form.addEventListener("submit", submitForm);
