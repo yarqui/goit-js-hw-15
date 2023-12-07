4588ddc58c2562865fc2cc3c8a3470d38c3cde1d
yarqui <y.pelykh@gmail.com>
2023-12-06 20:48:06 -0500

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,22 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+const refs = {
+  body: document.querySelector("body"),
+  changeColorBtn: document.querySelector("button.change-color"),
+  outputText: document.querySelector("span.color"),
+};
+
+const getRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+
+const changeBGColor = (color) => {
+  refs.body.style.backgroundColor = color;
+  refs.outputText.textContent = color;
+};
+
+const onColorChange = () => {
+  const color = getRandomHexColor();
+  changeBGColor(color);
+};
+
+refs.changeColorBtn.addEventListener("click", onColorChange);
