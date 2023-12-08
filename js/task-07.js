41a681e1299ff27b3b551a567313de1685989db4
yarqui <y.pelykh@gmail.com>
2023-12-07 21:46:36 -0500

    edit task 07

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,11 @@
+const MIN_FONT_SIZE = refs.rangeControl.getAttribute("min");
+
 const refs = {
   rangeControl: document.querySelector("input#font-size-control"),
   outputText: document.querySelector("span#text"),
 };
+
+refs.rangeControl.value = MIN_FONT_SIZE;
 
 refs.rangeControl.addEventListener("input", (e) => {
   refs.outputText.style.fontSize = `${e.currentTarget.value}px`;
